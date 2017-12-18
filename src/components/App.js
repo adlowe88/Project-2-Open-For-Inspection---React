be88c350c72c1222ba4766a1b785ec823ec7aac9
BingDarla <hbshuang@gmail.com>
2017-12-18 12:41:33 +1100

    Price predict component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import OpenForInspection from './OpenForInspection';
 
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+
+
       </div>
     );
   }
