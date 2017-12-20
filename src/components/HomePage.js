ee92d6670c9c382beff5112363ae56b06e456646
ezra mizrahi <mishraki@gmail.com>
2017-12-20 14:45:07 +1100

    homepage component added

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { PureComponent as Component } from 'react';
+import Login from './Login';
+import OpenForInspection from './OpenForInspection';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <p>homepage goes here</p>
+    );
+  }
+}
+
+export default HomePage;
