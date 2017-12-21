efd76a7eebcb086d31da9c40bcbe8d3351fee81a
ezra mizrahi <mishraki@gmail.com>
2017-12-21 11:10:00 +1100

    route corrections

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
-<<<<<<< HEAD
-import OpenForInspection from './components/OpenForInspection';
-import Property from './components/Property';
-import Search from './components/Search';
-import Results from './components/Results';
-import ProdictPrice from './components/PredictPrice';
-import Auction from './components/Auction';
-=======
+
 import Search from './components/PropertySearchPage/Search';
 import Property from './components/PropertyCreatePage/Property';
 import HomePage from './components/Homepage/HomePage';
->>>>>>> db4960660bf85d9fcd2e411b6822c32d0f25cc6b
+import Auction from './components/Auction';
+import PredictPrice from './components/Homepage/PredictPrice';
+
 
 
 const Routes = (
@@ -21,7 +16,7 @@ const Routes = (
       <Route exact path="/" component={ HomePage }/>
       <Route exact path="/property" component={ Property }/>
       <Route exact path="/search" component={ Search }/>
-      <Route exact path="/predict" component={ ProdictPrice }/>
+      <Route exact path="/predict" component={ PredictPrice }/>
       <Route exact path="/auction" component={ Auction }/>
     </div>
   </Router>
